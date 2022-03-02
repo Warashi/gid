4ae733c5f17c54764191db165c84a31cbd549669
Shinnosuke Sawada <6warashi9@gmail.com>
2022-03-03 08:43:40 +0900

    feat: initial commit

---
+++ goimports_test.go
@@ -0,0 +1,16 @@
+package goimports_test
+
+import (
+	"testing"
+
+	"github.com/Warashi/goimports-custom"
+
+	"github.com/gostaticanalysis/testutil"
+	"golang.org/x/tools/go/analysis/analysistest"
+)
+
+// TestAnalyzer is a test for Analyzer.
+func TestAnalyzer(t *testing.T) {
+	testdata := testutil.WithModules(t, analysistest.TestData(), nil)
+	analysistest.RunWithSuggestedFixes(t, testdata, goimports.Analyzer, "a")
+}
